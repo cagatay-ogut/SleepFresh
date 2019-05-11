d3579f8966d7cc280edb341252aa717f4fd71c5f
cagatay-ogut <ogutcagatay@gmail.com>
2019-05-11 15:33:32 +0300

    Show a notification with sound and vibration when alarm is received

--- app/src/main/java/com/cagatay/sleepfresh/AlarmReceiver.java
+++ app/src/main/java/com/cagatay/sleepfresh/AlarmReceiver.java
@@ -1,14 +1,39 @@
 package com.cagatay.sleepfresh;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Alarm ringing", Toast.LENGTH_LONG).show();
+        showAlarmNotification(context);
+    }
+
+    private void showAlarmNotification(Context context) {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(context, MainActivity.NOTIF_CHANNEL_ID)
+                        .setContentTitle(context.getString(R.string.notification_alarm_message))
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM))
+                        .setContentIntent(PendingIntent.getActivity(context, 0, new Intent(), 0))
+                        .setOngoing(true)
+                        .setVibrate(new long[] {500, 500})
+                        .setAutoCancel(true);
+
+        Notification notification = builder.build();
+        notification.flags |= Notification.FLAG_INSISTENT;
+
+        NotificationManagerCompat notManager = NotificationManagerCompat.from(context);
+        notManager.notify(1, notification);
     }
 }
--- app/src/main/java/com/cagatay/sleepfresh/MainActivity.java
+++ app/src/main/java/com/cagatay/sleepfresh/MainActivity.java
@@ -1,13 +1,41 @@
 package com.cagatay.sleepfresh;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.RingtoneManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String NOTIF_CHANNEL_ID = "NOTIF_CHANNEL_ALARM";
+    private final CharSequence NOTIF_CHANNEL_DESC = "Alarm Notification";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            AudioAttributes audioAttr = new AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_ALARM)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .setLegacyStreamType(AudioManager.STREAM_ALARM)
+                            .build();
+
+            NotificationChannel channel = new NotificationChannel(NOTIF_CHANNEL_ID, NOTIF_CHANNEL_DESC, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM), audioAttr);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
